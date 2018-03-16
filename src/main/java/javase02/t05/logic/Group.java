50060bb6bc79ac59a753d0456c90a43a12e4ae71
Sergey Bayramov <linferny@gmail.com>
2018-03-16 12:17:22 +0300

    HW-2 Task 5 started Before builder added

---
+++ src/main/java/javase02/t05/logic/Group.java
@@ -0,0 +1,5 @@
+package javase02.t05.logic;
+
+public class Group<T extends Number> {
+
+}
---
+++ src/main/java/javase02/t05/logic/Student.java
@@ -0,0 +1,9 @@
+package javase02.t05.logic;
+
+import java.util.HashMap;
+
+public class Student {
+    private String name;
+    private String surname;
+    private String patronymic;
+}
